f9532b8ab662034b852c7b488b3a05e18e824038
Jonny Williams <161750750+JonWilla@users.noreply.github.com>
2026-08-11 12:31:56 -0400

    feat: dockerize full stack application

--- backend/src/main/java/com/jonwilla/disasterresponse/security/SecurityConfig.java
+++ backend/src/main/java/com/jonwilla/disasterresponse/security/SecurityConfig.java
@@ -92,7 +92,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(
                 List.of(
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "http://localhost:3000"
                 )
         );
 
