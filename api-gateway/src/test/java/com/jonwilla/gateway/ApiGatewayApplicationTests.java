d84d06f03a1d3f0aadf14a13f18056076b43d9ea
Jonny Williams <161750750+JonWilla@users.noreply.github.com>
2026-08-14 18:15:45 -0400

    feat: add reactive API gateway for microservice routing

---
+++ api-gateway/src/test/java/com/jonwilla/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonwilla.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
